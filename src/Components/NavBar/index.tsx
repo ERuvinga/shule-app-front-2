679e050bdc65b95038966c0ea2f5bff9efcb4538
ERuvinga <ruvingaelie@gmail.com>
2023-08-09 16:24:55 +0200

    ajuste style

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -5,15 +5,15 @@ import {UserGroupIcon} from '@heroicons/react/24/outline'
 const navbar = ()=>{
     return(
         <nav className='Nav_bar'>
-            <div className='border'>
-                <span className='logo border'>Logo</span>
-                <ul className=' links border'>
+            <div className=''>
+                <span className='logo'>Logo</span>
+                <ul className=' links '>
                     <li><a className='link_selected' href='#'>accueil</a></li>
                     <li><a href='#'>valve</a></li>
                     <li><a href='#'>a propos</a></li>
                     <li><a href='#'>Grille</a></li>
                 </ul>
-                <div className='btns border'>
+                <div className='btns'>
                     <ul className='icons'>
                         <li><ChatBubbleBottomCenterTextIcon className='w-[25px]'/></li>
                         <li><UserGroupIcon className='w-[25px]'/></li>
