d6b7b409c8c1ce628796b3011cd8c8136537baca
ERuvinga <ruvingaelie@gmail.com>
2023-08-17 14:46:32 +0200

    + responsive design on NavBar component

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,27 +1,63 @@
-import {QuestionMarkCircleIcon, ChatBubbleBottomCenterTextIcon, UserGroupIcon} from '@heroicons/react/24/outline'
+import {QuestionMarkCircleIcon, ChatBubbleBottomCenterTextIcon, UserGroupIcon, Bars3Icon, XMarkIcon} from '@heroicons/react/24/outline';
+import { useEffect, useState } from 'react';
 
-const navbar = () => {
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [Width_screen, setWidth_screen] = useState(0);
+    const BreakPointNav = 850;
+
+    const toggleMenuScreen = ()=>{
+        setToggleMenu(!toggleMenu);
+    };
+
+    const onChangeWidth = () =>{
+        setWidth_screen(window.innerWidth);
+
+        if(window.innerWidth < BreakPointNav){
+            setToggleMenu(false);
+        }
+    }
+
+    useEffect(()=>{
+        setWidth_screen(window.innerWidth);
+        window.addEventListener('resize', onChangeWidth);
+        return () =>{
+            window.removeEventListener('resize', onChangeWidth);
+        }
+    },[]);
+
     return(
         <nav className='Nav_bar'>
-            <div className=''>
-                <span className='logo'>Logo</span>
-                <ul className=' links '>
-                    <li><a className='link_selected' href='#'>accueil</a></li>
-                    <li><a href='#'>valve</a></li>
-                    <li><a href='#'>a propos</a></li>
-                    <li><a href='#'>Grille</a></li>
-                </ul>
-                <div className='btns'>
-                    <ul className='icons'>
-                        <li><ChatBubbleBottomCenterTextIcon className='w-[25px]'/></li>
-                        <li><UserGroupIcon className='w-[25px]'/></li>
-                        <li><QuestionMarkCircleIcon className='w-[25px]'/></li>
-                    </ul>
-                    <a className='link_to_login' href='/Login'>Connexion</a>
-                </div>
+            <div>
+                <span className='logo'>
+                    <span>Logo</span>
+                    {toggleMenu ? <XMarkIcon className='w-[25px] btn_menu' onClick={toggleMenuScreen}/> : <Bars3Icon className='w-[25px] btn_menu' onClick={toggleMenuScreen}/>}
+                </span>
+                {
+                    (toggleMenu || (Width_screen > BreakPointNav)) && (
+                    <>
+                         <ul className=' links '>
+                            <li><a className='link_selected' href='#'>accueil</a></li>
+                            <li><a href='#'>valve</a></li>
+                            <li><a href='#'>a propos</a></li>
+                            <li><a href='#'>Grille</a></li>
+                        </ul>
+                        <div className='btns '>
+                            <ul className='icons '>
+                                <li><ChatBubbleBottomCenterTextIcon className='w-[25px]'/></li>
+                                <li><UserGroupIcon className='w-[25px]'/></li>
+                                <li><QuestionMarkCircleIcon className='w-[25px]'/></li>
+                            </ul>
+                            <a className='link_to_login' href='/Login'>Connexion</a>
+                        </div>
+                    </>
+                 ) 
+                
+                }
+                
             </div>
         </nav>
     )
 }
 
-export default navbar;
+export default Navbar;
