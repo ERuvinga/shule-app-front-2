653d590695d5773274e2aa8dcab50a43374e3bab
ERuvinga <ruvingaelie@gmail.com>
2023-08-23 12:48:07 +0200

    styling Login and Register component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,23 @@
 import { loginDataState } from "@/src/States"
 import {useSetRecoilState, useRecoilValue} from "recoil";
 import {useState} from 'react';
+import Loading from "@/src/Components/Loading";
+import InputField from "@/src/Components/InputField";
+import Footer from "@/src/Components/Footer";
 
 const index = ()=>{
     return(
         <>
+            <div>
+                <section className="login_form">
+                    <InputField labelText="email" placeholderText="neema@gmail.com" form_name="Login" type="text"/>
+                    <InputField labelText="password" placeholderText="******" form_name="Login" type="text"/>
+                    <InputField type="button" placeholderText="Login" form_name="Login"/>
+                </section>                
+            </div>
+            <Footer/>
         </>
+
     )
 };
 
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,10 +1,28 @@
 import { registerDateState } from "@/src/States";
 import { useState } from "react";
+import InputField from "@/src/Components/InputField";
+import Footer from "@/src/Components/Footer";
 
-const index = ()=>{
+interface proprietyInput {
+    placeholderText : String|any,
+    form_name: String|any,
+    type: String|any,
+    labelText?: String|any,
+}
+
+const index = (datas: proprietyInput)=>{
     return(
         <>
+            <div>
+                <section className="Register_form">
+                    <InputField labelText="Matricule" placeholderText="neema******" form_name="Register" type="text"/>
+                    <InputField labelText="password" placeholderText="******" form_name="Register" type="text"/>
+                    <InputField type="button" placeholderText="Register" form_name="Register"/>
+                </section>               
+            </div>
+            <Footer/>
         </>
+
     )
 };
 
