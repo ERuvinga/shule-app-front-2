4ed58982c5d65825029e2eb6dbcc52e06708db2a
ERuvinga <ruvingaelie@gmail.com>
2023-09-28 23:38:03 +0200

    filter of promotion component and Student CardUser

---
+++ src/Components/StudentCard/index.tsx
@@ -0,0 +1,26 @@
+import { monthNumberToString } from "@/src/Lib/Dates";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+
+interface StudentDatas{
+    name:String,
+    promotion:String,
+    DateUser:number,
+    statusCompte:boolean
+}
+const StudentsCardUser = (datas:StudentDatas) =>{
+
+    const InscriptionDate = new Date(datas.DateUser);
+    return(
+        <tr className="border">
+            <td><Image width={100} height={100} src={"/imgs/AuthImgs/avatar.png"} alt="directors Profil"/></td>
+            <td className="UserName">{datas.name}</td>
+            <td className="ClassPromotion">{datas.promotion}</td>
+            <td className="Tel">{`${InscriptionDate.getDate()} ${monthNumberToString(InscriptionDate.getMonth())} ${InscriptionDate.getFullYear()}`}</td>
+            <td><span className={datas.statusCompte? "actifAccount":"actifAccountFalse"}>{(datas.statusCompte)?"Oui":"Non"}</span></td>
+            <td><EllipsisVerticalIcon className="Icone"/></td>  
+        </tr>
+    )
+}
+
+export default StudentsCardUser;
